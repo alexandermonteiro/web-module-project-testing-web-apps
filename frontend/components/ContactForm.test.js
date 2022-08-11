a96b2dc082301047feab5efc1cf63ce986e960fd
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-08-10 17:05:45 -0700

    firstname&error

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -18,7 +18,15 @@ test("renders the contact form header", () => {
   expect(headerElement).toHaveTextContent(/contact form/i);
 });
 
-test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {});
+test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
+  render(<ContactForm />);
+
+  const firstNameField = screen.getByLabelText(/First Name*/i);
+  userEvent.type(firstNameField, "123");
+
+  const errorMessages = await screen.findAllByTestId("error");
+  expect(errorMessages).toHaveLength(1);
+});
 
 test("renders THREE error messages if user enters no values into any fields.", async () => {});
 
